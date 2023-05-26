d6dfd8cef91cf3d18275c7f4aef64699bd3dfafc
Bhupendra Vaishnav <83813377+bhupendra1610@users.noreply.github.com>
2023-05-26 11:21:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ if st.button("Frequent Items"):
   frequent_items=find_frequent_items(support)
   st.success('Apriori has found Frequent itemsets {}'.format(frequent_items))      
 if st.button("About"):
-  st.subheader("Developed by Deepak Moud")
-  st.subheader("Head , Department of Computer Engineering")
+  st.subheader("Developed by Bhupendra Vaishnav")
+  st.subheader("Student 3rd Year, PIET")
 html_temp = """
    <div class="" style="background-color:orange;" >
    <div class="clearfix">           
